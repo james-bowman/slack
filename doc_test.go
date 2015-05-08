472066c5f46f63f4a038e5c9a9f42e1bb686fe8c
James Bowman <james.edward.bowman@gmail.com>
2015-05-08 18:11:16 +0100

    added

---
+++ doc_test.go
@@ -0,0 +1,22 @@
+// Package slack allows integration with the Slack Real Time Messaging API (https://api.slack.com/rtm)
+// for consuming and publishing messages in real time to/from Slack (https://slack.com).
+//
+// To make use of the API, you will need to setup a user account or a 'bot' user
+// (https://api.slack.com/bot-users) (a special kind of user account specifically designed for
+// programatic access to APIs) and obtain an authentication token (https://api.slack.com/web#basics)
+// from Slack.
+package slack
+
+func Example() {
+	conn, err := slack.Connect(slackToken)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	replier := func(msg *slack.Message) {
+		msg.Respond(msg.Text)
+	}
+
+	slack.EventProcessor(conn, replier, nil)
+}
